1b1331f0dc865206732fcef3f965cceca8744cc2
Natalia <nataliya.yanovic@mail.ru>
2026-06-03 09:33:15 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const revolverTexto = (texto) => texto.split("").reverse().join("");
+
+const inputEl = document.getElementById("inputTexto");
+const btnEl = document.getElementById("btnCalcular");
+const resultEl = document.getElementById("resultadoTexto");
+
+btnEl.addEventListener("click", () => {
+    const texto = inputEl.value.trim();
+
+    if (texto === "") {
+        resultEl.textContent = "Error: ¡Los campos no pueden estar vacío!";        
+        resultEl.style.color = "red";
+        return; 
+    }
+
+    resultEl.textContent = `${revolverTexto(texto)}`;
+    resultEl.style.color = "white";
+
+    inputEl.value = "";
+    inputEl.focus();
+});
